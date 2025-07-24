0c7b2abe8dd434670c41f767ebf2eabcd2ebf802
nanaelie <elienana92@gmail.com>
2025-07-24 22:30:35 +0000

    feat: version centralisée du module Masonry

--- dist/masonry.js
+++ dist/masonry.js
@@ -29,6 +29,7 @@
     function masonry({ col, renderItems, container, spaceX = 1, spaceY = 1, debug = false }) {
         let nItemByCols = renderItems.length / col;
         let nLastItemCols = nItemByCols;
+        let last = 0;
         makeSpaceX(spaceX, container);
         let cols = [];
         let itemsCounter = initItemsCounter(col);
@@ -42,9 +43,10 @@
             renderItems[i].style.transition = 'all 0.3s ease';
             cols[i % col].appendChild(renderItems[i]);
             itemsCounter[i % col] += 1;
+            last = i % col;
         }
-        for (let i in cols) {
-            $(`${container}`).appendChild(cols[i]);
+        for (let colElement of cols) {
+            $(`${container}`).appendChild(colElement);
         }
         if (debug) {
             console.log('terminé');
@@ -53,6 +55,12 @@
             nLastItemCols != nItemByCols ? console.log(` - The first column has ${nLastItemCols} rows`) : '';
             console.log(` - ItemsCounter = ${itemsCounter}`);
         }
+        return {
+            append(item) {
+                cols[last].append(item);
+                $(`${container}`).appendChild(cols[last]);
+            }
+        };
     }
 
     return masonry;
