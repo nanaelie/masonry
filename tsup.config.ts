170b4c0c04ccb4c67eb1d992ba22816775df0d4a
Nana Elie <elienana92@gmail.com>
2026-06-16 12:57:41 +0000

    chore: touch tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs", "iife"],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    external: ["react", "react-dom"],
+    globalName: "MasonryView",
+    target: "es2018"
+});
